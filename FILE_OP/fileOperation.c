8c69d59b55e5a1857f2e13414227f5c29a638876
andoo <andoo@yu.com>
2022-10-26 16:43:21 +0800

    10.26 C语言学习

--- FILE_OP/fileOperation.c
+++ FILE_OP/fileOperation.c
@@ -136,25 +136,131 @@
 // }
 
 //  fscanf
-#include <stdio.h>
-struct S
-{
-    float score;
-    int n;
-    char arr[10];
-};
+// #include <stdio.h>
+// struct S
+// {
+//     float score;
+//     int n;
+//     char arr[10];
+// };
 
+// int main()
+// {
+//     struct S s = {0};
+//     FILE *pf = fopen("test.txt", "r");
+//     if (pf == NULL)
+//     {
+//         return 0;
+//     }
+//     // 格式化形式输入文件
+//     fscanf(pf, "%f%d%s",&(s.score),&(s.n),&(s.arr));
+//     printf("%f%d%s",s.score,s.n,s.arr);
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+
+// #include <stdio.h>
+// struct S
+// {
+//     char name[20];
+//     int n;
+//     double score;
+
+// };
+
+// int main()
+// {
+//     // 二进制写文件
+//     struct S s = {"张三", 20, 55.6};
+//     FILE *pf = fopen("test.txt", "wb");
+//     if (pf == NULL)
+//     {
+//         return 0;
+//     }
+//     // 二进制形式写文件
+//     fwrite(&s, sizeof(struct S), 1, pf);
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+
+// #include <stdio.h>
+// struct S
+// {
+//     char name[20];
+//     int age;
+//     double score;
+
+// };
+
+// int main()
+// {
+//     // 二进制读文件
+//     // struct S s = {"张三", 20, 55.6};
+//     struct S tmp = {0};
+
+//     FILE *pf = fopen("test.txt", "rb");
+//     if (pf == NULL)
+//     {
+//         return 0;
+//     }
+//     // 二进制形式 读文件
+//     fread(&tmp,sizeof(struct S), 1, pf);
+//     printf("%s %d %lf\n", tmp.name, tmp.age, tmp.score);
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     FILE *pf = fopen("test.txt", "r");
+//     if (pf == NULL)
+//     {
+//         return 0;
+//     }
+//     // 1.定位文件指针
+//     fseek(pf, 1, SEEK_CUR);   // 文件名 偏移量 文件指针当前位置(
+//         //SEEK_CUR 文件指针当前位置 SEEK_END 末尾位置   EEK_SET 文件起始位置
+//     //2，读取文件
+//     int ch = fgetc(pf);
+//     printf("%c\n",ch);
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main()
+// {
+//     FILE *pf = fopen("test.txt", "r");
+//     if (pf == NULL)
+//     {
+//         return 0;
+//     }
+//     // 1.定位文件指针
+//     fseek(pf, -2, SEEK_END);
+//     int pos = ftell(pf); // 计算偏移量
+//     printf("%d\n",pos);
+//     fclose(pf);
+//     pf = NULL;
+//     return 0;
+// }
+
+#include <stdio.h>
 int main()
 {
-    struct S s = {0};
+    // feof(); //EOF-end of file - 文件结束标志
+    // 不能用feof函数的返回值判断文件是否结束
+    // 应用于读取结束后判断是否读取失败结束，还是遇到文件尾结束
     FILE *pf = fopen("test.txt", "r");
     if (pf == NULL)
     {
         return 0;
     }
-    // 格式化形式输入文件
-    fscanf(pf, "%f%d%s",&(s.score),&(s.n),&(s.arr));
-    printf("%f%d%s",s.score,s.n,s.arr);
+   
     fclose(pf);
     pf = NULL;
     return 0;
