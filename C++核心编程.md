c2941e031343090135fa824dc7f3966e374a9568
andoo <andoo@yu.com>
2023-01-07 18:35:05 +0800

    继承

--- C++核心编程.md
+++ C++核心编程.md
@@ -2005,7 +2005,7 @@ public:
 protected:
     int m_B;
 private:
-    int m_C; 
+    int m_C;
 };
 
 class Son : public Base
@@ -2026,3 +2026,254 @@ int main()
     return 0;
 }
 ```
+
+#### 4.6.4 继承中构造和析构函数顺序
+
+```C++
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base构造函数!" << endl;
+    }
+    ~Base()
+    {
+        cout << "Base析构函数！" << endl;
+    }
+};
+class Son : public Base
+{
+public:
+    Son()
+    {
+        cout << "Son构造函数!" << endl;
+    }
+    ~Son()
+    {
+        cout << "Son析构函数！" << endl;
+    }
+};
+void test01()
+{
+    // Base b;
+    Son s;
+}
+int main()
+{
+    test01();
+    return 0;
+}
+```
+
+构造函数 父亲 儿子
+析构函数 儿子 父亲
+
+#### 4.6.5 继承同名成员处理方式
+
+- 访问子类同名函数 直接访问即可
+- 访问父类同名函数 需要加作用域
+
+```C++
+#include <iostream>
+using namespace std;
+// 继承中同名函数处理方式
+class Base
+{
+public:
+    Base()
+    {
+        m_A = 100;
+    }
+    void func()
+    {
+        cout << "Base - func()调用" << endl;
+    }
+    void func(int a)
+    {
+        cout << "Base - func(int a)调用" << endl;
+    }
+
+    int m_A;
+
+};
+class Son : public Base
+{
+public:
+    Son()
+    {
+       m_A = 200;
+    }
+    void func()
+    {
+        cout << "Son - func()调用" << endl;
+    }
+    int m_A;
+};
+void test01()
+{
+    Son s;
+    cout << "m_A="<< s.m_A << endl; // 访问子类
+    cout << "Base m_A = " << s.Base::m_A << endl; // 访问父类 加作用域
+}
+void test02()
+{
+    Son s;
+    s.func();
+    s.Base::func();//同名函数加作用域
+    // s.func(100); // 如果子类出现父类同名的成员函数，子类会隐藏父类所有的同名函数
+    s.Base::func(100);// 若访问 需要加作用域
+}
+int main()
+{
+    // test01();
+    test02();
+    return 0;
+}
+```
+
+#### 4.6.6 继承同名静态成员的处理方式
+
+- 访问子类同名函数 直接访问即可
+- 访问父类同名函数 需要加作用域
+
+```C++
+#include <iostream>
+using namespace std;
+// 继承中静态同名函数处理方式
+class Base
+{
+public:
+    static int m_A;
+};
+int Base::m_A = 100;
+class Son : public Base
+{
+public:
+    static int m_A;
+};
+int Son::m_A = 200;
+// 同名静态成员属性
+void test01()
+{
+    // 1.通过对象访问
+    Son s;
+    cout << "Son m_A=" << s.m_A << endl;          // 访问子类
+    cout << "Base m_A = " << s.Base::m_A << endl; // 访问父类 加作用域
+    // 2.通过类名访问
+    cout << "类名访问Son:m_A=" << Son::m_A << endl;
+    // cout << "类名访问Base:m_A= " << Base::m_A << endl;
+    cout << "类名访问Base:m_A= " << Son::Base::m_A << endl; // 通过类名的方式访问父类作用域下
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
+```
+
+#### 4.6.7 多继承语法
+
+class 子类：继承方式 父类 1， 继承方式 父类 2
+
+C++实际开发中不建议用多继承
+
+```C++
+#include <iostream>
+using namespace std;
+// 多继承语法
+class Base1
+{
+public:
+    Base1()
+    {
+        m_A = 100;
+    }
+    int m_A;
+};
+class Base2
+{
+public:
+    Base2()
+    {
+        m_A = 200;
+    }
+    int m_A;
+};
+
+class Son : public Base1, public Base2
+{
+public:
+    Son()
+    {
+        m_C = 300;
+        m_D = 400;
+    }
+
+    int m_C;
+    int m_D;
+
+};
+void test01()
+{
+    Son s;
+    cout << "size of son = " << sizeof(s) << endl;
+    cout << "Base1 m_A = " << s.Base1::m_A << endl;
+    cout << "Base2 m_A = " << s.Base2::m_A << endl;
+}
+int main()
+{
+    test01();
+    return 0;
+}
+```
+
+#### 4.6.8 菱形继承
+
+菱形继承概念:
+两个派生类继承同一个基类
+又有某个类同时继承两个派生类
+
+```C++
+#include <iostream>
+using namespace std;
+// 菱形继承
+// 动物类
+class Animal
+{
+public:
+    int m_Age;
+};
+//羊类
+class Sheep : virtual public Animal{}; //利用虚继承解决这种问题
+// Animal 虚基类
+
+//骆驼
+class Tuo : virtual public Animal{};
+// 羊驼类
+class SheepTuo : public Sheep, public Tuo{};
+
+void test01()
+{
+    SheepTuo st;
+    st.Sheep::m_Age = 18;
+    st.Tuo::m_Age = 28;
+    // 菱形继承加以作用域区分
+    cout << "Sheep_m_Age = " << st.Sheep::m_Age << endl;
+    cout << "Tuo_m_Age = " << st.Tuo::m_Age << endl;
+    // 菱形数据有两份 ，造成资源浪费 利用虚继承解决这种问题
+}
+int main()
+{
+    test01();
+    return 0;
+}
+```
+
+vpter vbtable 虚拟指针
+v -virtual
+b -base
+ptr -pointer
--- ClassAndObject/inheritance.cpp
+++ ClassAndObject/inheritance.cpp
@@ -133,7 +133,7 @@
 // protected:
 //     int m_B;
 // private:
-//     int m_C; 
+//     int m_C;
 // };
 
 // class Son1 : public Base1
@@ -152,7 +152,7 @@
 // protected:
 //     int m_B;
 // private:
-//     int m_C; 
+//     int m_C;
 // };
 // class Son2 : protected Base2
 // {
@@ -170,7 +170,7 @@
 // protected:
 //     int m_B;
 // private:
-//     int m_C; 
+//     int m_C;
 // };
 // class Son3 : private Base3
 // {
@@ -209,31 +209,240 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// class Base
+// {
+// public:
+//     int m_A;
+// protected:
+//     int m_B;
+// private:
+//     int m_C;
+// };
+
+// class Son : public Base
+// {
+//     // 父类中所有非静态成员属性都会被继承
+// public:
+//     int m_D;
+// };
+
+// void test01()
+// {
+//     cout << "size of Son =" << sizeof(Son) << endl;
+// }
+
+// int main()
+// {
+//     test01();
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// class Base
+// {
+// public:
+//     Base()
+//     {
+//         cout << "Base构造函数!" << endl;
+//     }
+//     ~Base()
+//     {
+//         cout << "Base析构函数！" << endl;
+//     }
+// };
+// class Son : public Base
+// {
+// public:
+//     Son()
+//     {
+//         cout << "Son函数!" << endl;
+//     }
+//     ~Son()
+//     {
+//         cout << "Son析构函数！" << endl;
+//     }
+// };
+// void test01()
+// {
+//     // Base b;
+//     Son s;
+// }
+// int main()
+// {
+//     test01();
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// // 继承中同名函数处理方式
+// class Base
+// {
+// public:
+//     Base()
+//     {
+//         m_A = 100;
+//     }
+//     void func()
+//     {
+//         cout << "Base - func()调用" << endl;
+//     }
+//     void func(int a)
+//     {
+//         cout << "Base - func(int a)调用" << endl;
+//     }
+
+//     int m_A;
+
+// };
+// class Son : public Base
+// {
+// public:
+//     Son()
+//     {
+//        m_A = 200;
+//     }
+//     void func()
+//     {
+//         cout << "Son - func()调用" << endl;
+//     }
+//     int m_A;
+// };
+// void test01()
+// {
+//     Son s;
+//     cout << "m_A="<< s.m_A << endl; // 访问子类
+//     cout << "Base m_A = " << s.Base::m_A << endl; // 访问父类 加作用域
+// }
+// void test02()
+// {
+//     Son s;
+//     s.func();
+//     s.Base::func();//同名函数加作用域
+//     // s.func(100); // 如果子类出现父类同名的成员函数，子类会隐藏父类所有的同名函数
+//     s.Base::func(100);// 若访问 需要加作用域
+// }
+// int main()
+// {
+//     // test01();
+//     test02();
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// // 继承中静态同名函数处理方式
+// class Base
+// {
+// public:
+//     static int m_A;
+// };
+// int Base::m_A = 100;
+// class Son : public Base
+// {
+// public:
+//     static int m_A;
+// };
+// int Son::m_A = 200;
+// // 同名静态成员属性
+// void test01()
+// {
+//     // 1.通过对象访问
+//     Son s;
+//     cout << "Son m_A=" << s.m_A << endl;          // 访问子类
+//     cout << "Base m_A = " << s.Base::m_A << endl; // 访问父类 加作用域
+//     // 2.通过类名访问
+//     cout << "类名访问Son:m_A=" << Son::m_A << endl;
+//     // cout << "类名访问Base:m_A= " << Base::m_A << endl;
+//     cout << "类名访问Base:m_A= " << Son::Base::m_A << endl; // 通过类名的方式访问父类作用域下
+
+// }
+
+// int main()
+// {
+//     test01();
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// // 多继承语法
+// class Base1
+// {
+// public:
+//     Base1()
+//     {
+//         m_A = 100;  
+//     }
+//     int m_A;
+// };
+// class Base2
+// {
+// public:
+//     Base2()
+//     {
+//         m_A = 200;  
+//     }
+//     int m_A;
+// };
+
+// class Son : public Base1, publicBase2
+// {
+// public:
+//     Son()
+//     {
+//         m_C = 300;
+//         m_D = 400;
+//     }
+
+//     int m_C;
+//     int m_D;
+
+// };
+// void test01()
+// {
+//     Son s;
+//     cout << "size of son = " << sizeof(s) << endl; 
+//     cout << "Base1 m_A = " << s.Base1::m_A << endl;     
+//     cout << "Base2 m_A = " << s.Base2::m_A << endl;    
+// }
+// int main()
+// {
+//     test01();
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
-
-class Base
+// 菱形继承
+// 动物类
+class Animal
 {
 public:
-    int m_A;
-protected:
-    int m_B;
-private:
-    int m_C; 
+    int m_Age;
 };
+//羊类
+class Sheep : virtual public Animal{}; //利用虚继承解决这种问题
+// Animal 虚基类
 
-class Son : public Base
-{
-    // 父类中所有非静态成员属性都会被继承
-public:
-    int m_D;
-};
+//骆驼
+class Tuo : virtual public Animal{};
+// 羊驼类
+class SheepTuo : public Sheep, public Tuo{};
 
 void test01()
 {
-    cout << "size of Son =" << sizeof(Son) << endl;
+    SheepTuo st;
+    st.Sheep::m_Age = 18; 
+    st.Tuo::m_Age = 28; 
+    // 菱形继承加以作用域区分
+    cout << "Sheep_m_Age = " << st.Sheep::m_Age << endl;   
+    cout << "Tuo_m_Age = " << st.Tuo::m_Age << endl;
+    // 菱形数据有两份 ，造成资源浪费 利用虚继承解决这种问题  
 }
-
 int main()
 {
     test01();
